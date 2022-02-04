b43eb34b2454d28c2d75e55ee512bd665771aa5a
Ranga Rathnayake <58025781+RangaRathnayake@users.noreply.github.com>
2022-02-04 20:17:11 +0530

    update

--- src/customer/customer.controller.ts
+++ src/customer/customer.controller.ts
@@ -1,23 +1,23 @@
+/* eslint-disable prettier/prettier */
 import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { CustomerService } from './customer.service';
 
 @Controller('customer')
 export class CustomerController {
-    constructor(private customerService: CustomerService) { }
+  constructor(private customerService: CustomerService) {}
 
-    @Post('save')
-    async create(@Body('customer') customer) {
-        return await this.customerService.create(customer);
-    }
+  @Post('save')
+  async create(@Body('customer') customer) {
+    return await this.customerService.create(customer);
+  }
 
-    @Get('get')
-    async getAll() {
-        return await this.customerService.getAll();
-    }
+  @Get('get')
+  async getAll() {
+    return await this.customerService.getAll();
+  }
 
-    @Get(':id')
-    async getOne(@Param('id') id) {
-        return await this.customerService.getOne(id);
-    }
-
+  @Get(':id')
+  async getOne(@Param('id') id) {
+    return await this.customerService.getOne(id);
+  }
 }
